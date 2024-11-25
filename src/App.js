540c412f468edddb353ffd36a71c476116bcf95f
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-11-25 16:15:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import ImageComponent from './components/ImageComponent'
 
 function App() {
   return (
-    <div>
+    <>
       <ButtonComponent text="ciaooooo" />
       <ImageComponent
         url="https://media.autoaddicted.com/2017/05/2015-dodge-challenger-srt-hellcat-photo-615334-s-986x603-986x580.jpg"
         alt="Hellcat"
       />
-    </div>
+    </>
   )
 }
 
